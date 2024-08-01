d86d4a6e139955ce450d76ea5406281f51a88b50
Ali Hassan <alihassan478@icloud.com>
2024-08-01 21:28:53 +0500

    few tweaks

--- Backend/Backend-Apis/app/Http/Controllers/UserController.php
+++ Backend/Backend-Apis/app/Http/Controllers/UserController.php
@@ -10,7 +10,7 @@ class UserController extends Controller
     
     public function index()
     {
-        $users = User::where('role', 'end-user')
+        $users = User::where('role', 'end_user')
         ->with(['household.town'])  // Eager load `household` and `town`
         ->get();
 
--- Backend/Backend-Apis/routes/api.php
+++ Backend/Backend-Apis/routes/api.php
@@ -11,66 +11,68 @@ use App\Http\Controllers\TownController;
 use App\Http\Controllers\UserController;
 use App\Http\Middleware\AdminMiddleware;
 use App\Http\Middleware\TownAdminMiddleware;
-use App\Models\Complaint;
 use Illuminate\Support\Facades\Route;
 
-// routes/api.php
-
+// Public routes
 Route::post('/register', [AuthController::class, 'register']);
-Route::post('/add-details', [AuthController::class, 'addDetails'])->middleware('auth:sanctum');
-
 Route::post('/login', [AuthController::class, 'login']);
 
-Route::apiResource('/business', BusinessController::class);
-Route::apiResource('/products', ProductController::class);
-// this api point is for getting products related to specific restaurant
-Route::get('/all-products/{businessId}', [ProductController::class, 'index']);
-
-
-Route::group([
-    'middleware' => ['auth:sanctum']
-], function() {
+// Routes requiring authentication
+Route::middleware('auth:sanctum')->group(function() {
+    Route::post('/add-details', [AuthController::class, 'addDetails']);
+    
+    // Complaints
     Route::post('/complaints', [ComplainController::class, 'store']);
     
+    // Cart
+    Route::prefix('cart')->group(function() {
+        Route::post('/', [CartController::class, 'addToCart']);
+        Route::get('/', [CartController::class, 'viewCart']);
+        Route::delete('{id}', [CartController::class, 'removeCart']);
+        Route::delete('product/{id}', [CartController::class, 'removeProduct']);
+    });
+    
+    // Orders
+    Route::prefix('order')->group(function() {
+        Route::post('/', [OrderController::class, 'placeOrder']);
+        Route::get('/', [OrderController::class, 'viewOrders']);
+    });
+    
+    // Businesses and Products
+    Route::apiResource('/business', BusinessController::class);
+    Route::apiResource('/products', ProductController::class);
+    Route::get('/all-products/{businessId}', [ProductController::class, 'index']);
+    
+    // Towns
     Route::apiResource('/towns', TownController::class);
-    
-    
-    Route::post('cart', [CartController::class, 'addToCart']);
-    Route::get('cart', [CartController::class, 'viewCart']);
-    Route::delete('cart/{id}', [CartController::class, 'removeCart']);
-    Route::delete('product/{id}', [CartController::class, 'removeProduct']);
-    
-    
-    Route::post('order', [OrderController::class, 'placeOrder']);
-    Route::get('orders', [OrderController::class, 'viewOrders']);
 });
 
-
-// main admin
+// Admin routes
 Route::middleware(['auth:sanctum', AdminMiddleware::class])->prefix('/admin')->group(function() {
-
-    Route::post("/createAdmins", [AdminController::class, 'createTownAdmin']);
+    // Admins management
+    Route::post('/createAdmins', [AdminController::class, 'createTownAdmin']);
+    
+    // Users management
+    Route::prefix('/users')->group(function() {
+        Route::get('/', [UserController::class, 'index'])->name('admin.users.index');
+        Route::get('{id}', [UserController::class, 'show'])->name('admin.users.show');
+        Route::put('{id}', [UserController::class, 'update'])->name('admin.users.update');
+        Route::delete('{id}', [UserController::class, 'destroy'])->name('admin.users.destroy');
+    });
+    
+    // Towns
     Route::apiResource('/towns', TownController::class);
-
-    Route::get('/users', [UserController::class, 'index'])->name('admin.users.index');
-    Route::get('/users/{id}', [UserController::class, 'show'])->name('admin.users.show');
-    Route::put('/users/{id}', [UserController::class, 'update'])->name('admin.users.update');
-    Route::delete('/users/{id}', [UserController::class, 'destroy'])->name('admin.users.destroy');
-
 });
 
-
-// town admin
+// Town admin routes
 Route::middleware(['auth:sanctum', TownAdminMiddleware::class])->prefix('/restaurantAdmin')->group(function() {
-    
-    
+    // Complaints management
     Route::put('/complaint-status', [ComplainController::class, 'update']);
     Route::get('/complaints', [ComplainController::class, 'complaintsOfTown']);
-
-    Route::get('/allproducts', [ ProductController::class, 'getProducts']);
-
-    // Route::get('/orders', [OrderController::class, 'viewAllOrders']);
-
-    Route::get('/orders', [OrderController::class, 'viewRestaurantOrders']);
     
+    // Products management
+    Route::get('/allproducts', [ProductController::class, 'getProducts']);
+    
+    // Orders management
+    Route::get('/orders', [OrderController::class, 'viewRestaurantOrders']);
 });

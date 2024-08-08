6140fe63028d5e43738647ff3b84299eaf502e2b
Ali Hassan <alihassan478@icloud.com>
2024-08-08 22:24:20 +0500

    few changes

--- Backend/Backend-Apis/app/Models/Product.php
+++ Backend/Backend-Apis/app/Models/Product.php
@@ -10,7 +10,7 @@ class Product extends Model
 {
     use HasFactory, SoftDeletes;
 
-    protected $fillable = ['title', 'description', 'image', 'price', 'bussiness_id'];
+    protected $fillable = ['title', 'description', 'image', 'price', 'business_id'];
 
     protected $appends = ['image_url'];
 

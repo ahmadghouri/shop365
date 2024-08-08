1fcc36cf105fc23328529c98cca4595ba5dd930e
Ali Hassan <alihassan478@icloud.com>
2024-08-08 20:17:03 +0500

    done

--- Backend/Backend-Apis/app/Models/Product.php
+++ Backend/Backend-Apis/app/Models/Product.php
@@ -16,7 +16,7 @@ class Product extends Model
 
     public function getImageUrlAttribute()
     {
-        return url('' . $this->image);
+        return url('/be' . $this->image);
     }
 
     public function business(){

9d3bf22bda0d46e1e8acca6762b841f492527856
Ali Hassan <alihassan478@icloud.com>
2024-10-07 23:49:55 +0500

    delete working now

--- Backend/Backend-Apis/app/Http/Controllers/BusinessController.php
+++ Backend/Backend-Apis/app/Http/Controllers/BusinessController.php
@@ -1,94 +1,94 @@
-<?php
+    <?php
 
-namespace App\Http\Controllers;
+    namespace App\Http\Controllers;
 
-use App\Http\Requests\Business\StoreRequest;
-use App\Http\Requests\Business\UpdateRequest;
-use App\Models\Business;
-use App\Services\BusinessService;
-use App\Services\ImageService;
-use Exception;
-use Illuminate\Container\Attributes\DB;
-use Illuminate\Http\JsonResponse;
-use Illuminate\Http\Request;
-use Illuminate\Support\Facades\DB as FacadesDB;
-use Illuminate\Support\Facades\File;
-use Illuminate\Support\Str;
+    use App\Http\Requests\Business\StoreRequest;
+    use App\Http\Requests\Business\UpdateRequest;
+    use App\Models\Business;
+    use App\Services\BusinessService;
+    use App\Services\ImageService;
+    use Exception;
+    use Illuminate\Container\Attributes\DB;
+    use Illuminate\Http\JsonResponse;
+    use Illuminate\Http\Request;
+    use Illuminate\Support\Facades\DB as FacadesDB;
+    use Illuminate\Support\Facades\File;
+    use Illuminate\Support\Str;
 
-class BusinessController extends Controller
-{
-    protected $businessService;
-    protected $imageService;
-
-    public function __construct(BusinessService $businessService, ImageService $imageService)
+    class BusinessController extends Controller
     {
-        $this->businessService = $businessService;
-        $this->imageService = $imageService;
-    }
-    /**
-     * Display a listing of the resource.
-     */
-    public function index(): JsonResponse
-    {
-        $businesses = Business::all();
+        protected $businessService;
+        protected $imageService;
 
-        return $this->successResponse($businesses, 'All the businesses');
-    }
+        public function __construct(BusinessService $businessService, ImageService $imageService)
+        {
+            $this->businessService = $businessService;
+            $this->imageService = $imageService;
+        }
+        /**
+         * Display a listing of the resource.
+         */
+        public function index(): JsonResponse
+        {
+            $businesses = Business::all();
 
-    /**
-     * Store a newly created resource in storage.
-     */
-    public function store(StoreRequest $request): JsonResponse
-    {
-        $business = $this->businessService->store($request->validated());
-
-        if ($request->has('image')) {
-            $imagePath = $this->imageService->uploadImage($request, 'image');
-            $business->image = $imagePath;
-            $business->save();
+            return $this->successResponse($businesses, 'All the businesses');
         }
 
-        return $this->successResponse($business, 'Business added successfully');
-    }
+        /**
+         * Store a newly created resource in storage.
+         */
+        public function store(StoreRequest $request): JsonResponse
+        {
+            $business = $this->businessService->store($request->validated());
 
-    /**
-     * Display the specified resource.
-     */
-    public function show(Business $business): JsonResponse
-    {
-        return $this->successResponse($business, 'Business', 201);
-    }
+            if ($request->has('image')) {
+                $imagePath = $this->imageService->uploadImage($request, 'image');
+                $business->image = $imagePath;
+                $business->save();
+            }
 
-    /**
-     * Update the specified resource in storage.
-     */
-    public function update(UpdateRequest $request, Business $business): JsonResponse
-    {
-        $business->update($request->only(['type', 'name', 'opening_time', 'closing_time','image']));
-
-        if ($request->hasFile('image')) {
-            $imagePath = $this->imageService->uploadImage($request, 'image');
-            $business->image = $imagePath;
-            $business->save();
+            return $this->successResponse($business, 'Business added successfully');
         }
 
-        return $this->successResponse($business->refresh(), 'Updated');
-    }
-
-    /**
-     * Remove the specified resource from storage.
-     */
-    public function destroy($id): JsonResponse
-    {
-        try {
-            $this->businessService->delete($id);
-            return $this->successResponse(null, 'Deleted Successfully');
-        } catch (\Illuminate\Database\Eloquent\ModelNotFoundException $e) {
-            return $this->errorResponse('Business not found', 404);
-        } catch (\Exception $e) {
-            return $this->errorResponse('An error occurred: ' . $e->getMessage(), 500);
+        /**
+         * Display the specified resource.
+         */
+        public function show(Business $business): JsonResponse
+        {
+            return $this->successResponse($business, 'Business', 201);
         }
-    }
+
+        /**
+         * Update the specified resource in storage.
+         */
+        public function update(UpdateRequest $request, Business $business): JsonResponse
+        {
+            $business->update($request->only(['type', 'name', 'opening_time', 'closing_time','image']));
+
+            if ($request->hasFile('image')) {
+                $imagePath = $this->imageService->uploadImage($request, 'image');
+                $business->image = $imagePath;
+                $business->save();
+            }
+
+            return $this->successResponse($business->refresh(), 'Updated');
+        }
+
+        /**
+         * Remove the specified resource from storage.
+         */
+        public function destroy($id): JsonResponse
+        {
+            try {
+                $this->businessService->delete($id);
+                return $this->successResponse(null, 'Deleted Successfully');
+            } catch (\Illuminate\Database\Eloquent\ModelNotFoundException $e) {
+                return $this->errorResponse('Business not found', 404);
+            } catch (\Exception $e) {
+                return $this->errorResponse('An error occurred: ' . $e->getMessage(), 500);
+            }
+        }
 
 
     public function getBusinessStats(Request $request)

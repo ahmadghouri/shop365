9c6dd61a4725be3dd65145db8fea4af5e13bbe60
Ali Hassan <alihassan478@icloud.com>
2024-10-19 15:47:01 +0500

    new changes

--- Frontend/src/pages/Admin/Dashboard.vue
+++ Frontend/src/pages/Admin/Dashboard.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="bg-gray-100 min-h-screen">
+  <div class="bg-gray-100 min-h-screen mobile-spacing">
     <div class="mb-6 flex items-center justify-between">
       <h1 class="text-2xl font-bold text-gray-800">All Restaurants</h1>
       <button

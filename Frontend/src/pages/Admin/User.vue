b33bf68bb3a4127f53d70942f5adda3ef84f89d4
Ali Hassan <alihassan478@icloud.com>
2024-10-22 20:27:21 +0500

    user and admin deletion

--- Frontend/src/pages/Admin/User.vue
+++ Frontend/src/pages/Admin/User.vue
@@ -103,6 +103,37 @@
               {{ user.household?.town?.town_name || "No Town" }}
             </p>
           </div>
+
+          <button
+            @click="showDeleteConfirmation(user.id)"
+            class="mt-4 bg-red-500 text-white py-1 px-3 rounded hover:bg-red-600"
+          >
+            Delete
+          </button>
+        </div>
+      </div>
+    </div>
+
+    <div
+      v-if="showDeleteConfirm"
+      class="fixed inset-0 flex items-center justify-center bg-gray-900 bg-opacity-50"
+    >
+      <div class="bg-white p-6 rounded-lg shadow-xl">
+        <h2 class="text-xl font-bold mb-4">Confirm Deletion</h2>
+        <p class="mb-4">Are you sure you want to delete this User?</p>
+        <div class="flex justify-end space-x-2">
+          <button
+            @click="confirmDelete"
+            class="bg-red-500 px-4 py-2 text-white rounded-md"
+          >
+            Delete
+          </button>
+          <button
+            @click="cancelDelete"
+            class="bg-gray-300 px-4 py-2 text-gray-800 rounded-md"
+          >
+            Cancel
+          </button>
         </div>
       </div>
     </div>
@@ -112,9 +143,33 @@
 <script setup>
 import { ref, computed, onMounted } from "vue";
 import { useUserStore } from "../../store/userStore";
+import { toast } from "vue3-toastify";
 
 const userStore = useUserStore();
 const sortOrder = ref("desc");
+const showDeleteConfirm = ref(false);
+const userId = ref("");
+
+const token = localStorage.getItem("token");
+
+const showDeleteConfirmation = (id) => {
+  userId.value = id;
+  showDeleteConfirm.value = true;
+};
+
+const cancelDelete = () => {
+  showDeleteConfirm.value = false;
+  restaurantToDeleteId.value = null;
+};
+
+const confirmDelete = async () => {
+  if (userId.value) {
+    await userStore.deleteUser(userId.value);
+    showDeleteConfirm.value = false;
+    userId.value = null;
+    toast.success("Admin Deleted Successfully");
+  }
+};
 
 const fetchUsers = async () => {
   await userStore.getUsers();
@@ -143,6 +198,4 @@ const isTopThree = (index) => {
 };
 </script>
 
-<style scoped>
-/* Optional custom styles for further refinement */
-</style>
+<style scoped></style>

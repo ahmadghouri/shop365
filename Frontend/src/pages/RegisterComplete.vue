cf9ba16a4a57723d054d23e3523deb71edb91282
Ali Hassan <alihassan478@icloud.com>
2024-10-28 19:33:08 +0500

    skip button removed

--- Frontend/src/pages/RegisterComplete.vue
+++ Frontend/src/pages/RegisterComplete.vue
@@ -73,13 +73,6 @@
           >
             Register
           </button>
-          <button
-            type="button"
-            class="w-full border border-yellow-500 text-yellow-500 hover:bg-yellow-100 font-medium rounded-lg text-sm px-5 py-2.5 text-center"
-            @click="skip"
-          >
-            Skip
-          </button>
         </div>
         <p class="text-sm font-light text-gray-500 text-center">
           Already have an account?

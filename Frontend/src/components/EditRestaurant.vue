74c8f51fba5ddb523c6165184f5ca17817a98bff
Awais Jameel <awais.jameel@ymail.com>
2024-10-29 20:31:48 +0500

    Unnecessery code removved.

--- Frontend/src/components/EditRestaurant.vue
+++ Frontend/src/components/EditRestaurant.vue
@@ -4,71 +4,36 @@
     <form @submit.prevent="handleSubmit">
       <div class="mb-4">
         <label for="name" class="block text-gray-700">Name</label>
-        <input
-          v-model="form.name"
-          type="text"
-          id="name"
-          class="w-full p-2 border border-gray-300 rounded-md"
-          required
-        />
+        <input v-model="form.name" type="text" id="name" class="w-full p-2 border border-gray-300 rounded-md"
+          required />
       </div>
       <div class="mb-4">
         <label for="type" class="block text-gray-700">Type</label>
-        <input
-          v-model="form.type"
-          type="text"
-          id="type"
-          class="w-full p-2 border border-gray-300 rounded-md"
-          required
-        />
+        <input v-model="form.type" type="text" id="type" class="w-full p-2 border border-gray-300 rounded-md"
+          required />
       </div>
       <div class="mb-4">
-        <label for="opening_time" class="block text-gray-700"
-          >Opening Time</label
-        >
-        <input
-          v-model="form.opening_time"
-          type="text"
-          id="opening_time"
-          class="w-full p-2 border border-gray-300 rounded-md"
-          required
-        />
+        <label for="opening_time" class="block text-gray-700">Opening Time</label>
+        <input v-model="form.opening_time" type="text" id="opening_time"
+          class="w-full p-2 border border-gray-300 rounded-md" required />
       </div>
       <div class="mb-4">
-        <label for="closing_time" class="block text-gray-700"
-          >Closing Time</label
-        >
-        <input
-          v-model="form.closing_time"
-          type="text"
-          id="closing_time"
-          class="w-full p-2 border border-gray-300 rounded-md"
-          required
-        />
+        <label for="closing_time" class="block text-gray-700">Closing Time</label>
+        <input v-model="form.closing_time" type="text" id="closing_time"
+          class="w-full p-2 border border-gray-300 rounded-md" required />
       </div>
 
       <div class="mb-4">
         <label for="image" class="block text-gray-700">Image</label>
-        <input
-          @change="(e) => (form.image = e.target.files[0])"
-          type="file"
-          id="image"
-          class="w-full p-2 border border-gray-300 rounded-md"
-        />
+        <input @change="(e) => (form.image = e.target.files[0])" type="file" id="image"
+          class="w-full p-2 border border-gray-300 rounded-md" />
       </div>
 
       <div class="flex justify-end gap-2">
-        <button
-          @click="$emit('close')"
-          type="button"
-          class="bg-gray-500 px-4 py-2 text-white rounded-md"
-        >
+        <button @click="$emit('close')" type="button" class="bg-gray-500 px-4 py-2 text-white rounded-md">
           Cancel
         </button>
-        <button
-          type="submit"
-          class="bg-blue-500 px-4 py-2 text-white rounded-md"
-        >
+        <button type="submit" class="bg-blue-500 px-4 py-2 text-white rounded-md">
           Save
         </button>
       </div>
@@ -77,7 +42,7 @@
 </template>
 
 <script setup>
-import { ref, defineProps, defineEmits } from "vue";
+import { ref, } from "vue";
 import { useBusinessStore } from "../store/businessStore.js";
 
 const props = defineProps({

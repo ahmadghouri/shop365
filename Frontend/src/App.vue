e911935dec7fa4b3ea81bc9aa47a842eb7884561
Awais Jameel <awais.jameel@ymail.com>
2024-10-30 22:34:35 +0500

    Event triggers added.

--- Frontend/src/App.vue
+++ Frontend/src/App.vue
@@ -1,4 +1,21 @@
-<script setup></script>
+<script setup>
+import { onMounted } from 'vue';
+
+onMounted(() => {
+  document.addEventListener('customEvent', function (e) {
+    window.location.href = 'inapp://customEventTriggered?' + encodeURIComponent(JSON.stringify(e.detail));
+  });
+
+  setTimeout(() => {
+    document.dispatchEvent(new CustomEvent('customEvent', { detail: { key: 'test custom event: Application Loaded' } }));
+  }, 10000)
+
+  window?.webkit?.messageHandlers?.cordova_iab?.postMessage('Application Loaded');
+  window?.parent?.postMessage('Application Loaded');
+  window?.postMessage('Application Loaded');
+  console.log("Application Loaded",);
+})
+</script>
 
 <template>
   <router-view></router-view>

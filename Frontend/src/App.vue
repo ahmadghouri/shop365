0296b7355f26e2a2c7b6a3bdbc3709dd2a9646fb
Awais Jameel <awais.jameel@ymail.com>
2024-10-30 22:54:33 +0500

    incorrect protocol removved.

--- Frontend/src/App.vue
+++ Frontend/src/App.vue
@@ -2,13 +2,13 @@
 import { onMounted } from 'vue';
 
 onMounted(() => {
-  document.addEventListener('customEvent', function (e) {
-    window.location.href = 'inapp://customEventTriggered?' + encodeURIComponent(JSON.stringify(e.detail));
-  });
+  // document.addEventListener('customEvent', function (e) {
+  //   window.location.href = 'inapp://customEventTriggered?' + encodeURIComponent(JSON.stringify(e.detail));
+  // });
 
-  setTimeout(() => {
-    document.dispatchEvent(new CustomEvent('customEvent', { detail: { key: 'test custom event: Application Loaded' } }));
-  }, 10000)
+  // setTimeout(() => {
+  //   document.dispatchEvent(new CustomEvent('customEvent', { detail: { key: 'test custom event: Application Loaded' } }));
+  // }, 10000)
 
   window?.webkit?.messageHandlers?.cordova_iab?.postMessage('Application Loaded');
   window?.parent?.postMessage('Application Loaded');

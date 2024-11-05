2698e62fb1d8913c0ae3726839dcd7b1b655d4fa
Ali Hassan <alihassan478@icloud.com>
2024-11-05 21:46:53 +0500

    few changes

--- Frontend/src/pages/OrderHistory.vue
+++ Frontend/src/pages/OrderHistory.vue
@@ -5,21 +5,38 @@
       <!-- Back Arrow Button and Refresh Button -->
       <div class="flex justify-between items-center">
         <button @click="goBack" class="">
-          <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor"
-            class="w-4 h-4 text-gray-700 hover:text-gray-900">
-            <path stroke-linecap="round" stroke-linejoin="round" d="M15 19l-7-7 7-7" />
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke-width="2"
+            stroke="currentColor"
+            class="w-4 h-4 text-gray-700 hover:text-gray-900"
+          >
+            <path
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              d="M15 19l-7-7 7-7"
+            />
           </svg>
         </button>
 
         <h1 class="text-center text-xl font-semibold">Orders</h1>
-        <button @click="refreshOrders" class="bg-yellow-500 text-white px-4 py-2 rounded">
+        <button
+          @click="refreshOrders"
+          class="bg-yellow-500 text-white px-4 py-2 rounded"
+        >
           Refresh
         </button>
       </div>
     </div>
 
     <!-- Orders List -->
-    <div v-for="order in orderDetails" :key="order.id" class="max-w-full mt-6 bg-gray-200 rounded-lg p-4">
+    <div
+      v-for="order in orderDetails"
+      :key="order.id"
+      class="max-w-full mt-6 bg-gray-200 rounded-lg p-4"
+    >
       <div class="flex justify-between items-center mb-4">
         <div>
           <h1 class="font-semibold">Order ID: {{ order.id }}</h1>
@@ -28,32 +45,40 @@
           </p>
         </div>
 
-        <div :class="{
-          'bg-red-500 text-white text-sm rounded-full px-4 py-1 text-center':
-            order.status === 'pending',
-          'bg-yellow-500 text-white text-sm rounded-full px-4 py-1 text-center':
-            order.status === 'preparing',
-          'bg-green-500 text-white text-sm rounded-full px-4 py-1 text-center':
-            order.status === 'delivered',
-        }">
+        <div
+          :class="{
+            'bg-red-500 text-white text-sm rounded-full px-4 py-1 text-center':
+              order.status === 'pending',
+            'bg-yellow-500 text-white text-sm rounded-full px-4 py-1 text-center':
+              order.status === 'preparing',
+            'bg-green-500 text-white text-sm rounded-full px-4 py-1 text-center':
+              order.status === 'delivered',
+          }"
+        >
           {{ order.status }}
         </div>
       </div>
 
-      <p :class="{
-        'text-[12px] font-semibold text-red-500 italic':
-          order.status === 'pending',
-        'text-[12px] font-semibold text-yellow-500 italic':
-          order.status === 'preparing',
-        'text-[12px] font-semibold text-green-500 italic':
-          order.status === 'delivered',
-      }">
+      <p
+        :class="{
+          'text-[12px] font-semibold text-red-500 italic':
+            order.status === 'pending',
+          'text-[12px] font-semibold text-yellow-500 italic':
+            order.status === 'preparing',
+          'text-[12px] font-semibold text-green-500 italic':
+            order.status === 'delivered',
+        }"
+      >
         {{ getOrderStatusMessage(order.status) }}
       </p>
 
       <div class="mt-2 border-t border-gray-200">
         <p class="text-sm font-semibold text-gray-700">Order Items:</p>
-        <div v-for="item in order.items" :key="item.id" class="flex justify-between text-sm py-1">
+        <div
+          v-for="item in order.items"
+          :key="item.id"
+          class="flex justify-between text-sm py-1"
+        >
           <span>
             {{ item.product.title }}
             <span class="text-gray-500">x {{ item.quantity }}</span>
@@ -72,9 +97,37 @@
     </div>
   </div>
 
+  <div
+    v-else-if="!isLoading"
+    class="flex flex-col justify-center items-center h-screen bg-gray-100"
+  >
+    <div
+      class="bg-white shadow-md rounded-lg p-8 flex flex-col justify-center items-center"
+    >
+      <svg
+        class="w-16 h-16 text-gray-500 mb-4"
+        fill="none"
+        viewBox="0 0 24 24"
+        stroke="currentColor"
+      >
+        <path
+          stroke-linecap="round"
+          stroke-linejoin="round"
+          stroke-width="2"
+          d="M9 12l2 2 4-4m6 2a9 9 0 11-18 0 9 9 0 0118 0z"
+        />
+      </svg>
+      <h2 class="text-2xl font-semibold text-gray-700 mb-2">
+        No orders for today
+      </h2>
+    </div>
+  </div>
+
   <!-- Loading Spinner -->
   <div v-else class="flex justify-center items-center h-screen bg-gray-100">
-    <div class="animate-spin rounded-full h-16 w-16 border-t-2 border-b-2 border-yellow-500"></div>
+    <div
+      class="animate-spin rounded-full h-16 w-16 border-t-2 border-b-2 border-yellow-500"
+    ></div>
   </div>
 </template>
 
@@ -86,8 +139,10 @@ import { useOrderStore } from "../store/orderStore";
 const orderDetails = ref([]);
 const orderStore = useOrderStore();
 const router = useRouter();
+const isLoading = ref(true);
 
 async function getOrderDetails() {
+  isLoading.value = true;
   try {
     await orderStore.getOrderDetails();
     orderDetails.value = orderStore.userOrderDetails;
@@ -97,6 +152,8 @@ async function getOrderDetails() {
     );
   } catch (error) {
     console.error(error);
+  } finally {
+    isLoading.value = false;
   }
 }
 

834eccf754ecd7c69d35f89cbf14ab329469415f
Ali Hassan <alihassan478@icloud.com>
2024-11-15 03:07:27 +0500

    new

--- Backend/Backend-Apis/app/Http/Controllers/GroceryProductController.php
+++ Backend/Backend-Apis/app/Http/Controllers/GroceryProductController.php
@@ -37,7 +37,7 @@ class GroceryProductController extends Controller
             $products = $response->json(); // Assuming the data is in JSON format
 
             foreach ($products as $product) {
-                $existingProduct = Product::where('title', $product['ITEM_DESC'])->first();
+                $existingProduct = Product::where('title', $product['ITEM_DESC'])->where('price', $product['UNIT_PRICE'])->first();
 
                 if (!$existingProduct) {
                     Product::create([

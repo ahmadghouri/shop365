b0bd8f16b968c8ed3f49dee9c19e7d44d7e7122b
Ali Hassan <alihassan478@icloud.com>
2024-11-16 14:51:58 +0500

    cancell status added

--- Frontend/src/pages/RestaurantAdmin/RestaurantOrder.vue
+++ Frontend/src/pages/RestaurantAdmin/RestaurantOrder.vue
@@ -11,29 +11,50 @@
 
     <div class="mb-6 flex items-center gap-4">
       <div class="flex gap-3">
-        <button @click="selectedStatus = 'pending'" :class="[
-          'px-4 py-2 rounded-full text-sm font-medium transition-colors duration-300',
-          selectedStatus === 'pending'
-            ? 'bg-red-500 text-white'
-            : 'border-2 border-red-500 text-red-500 hover:bg-red-50',
-        ]">
+        <button
+          @click="selectedStatus = 'pending'"
+          :class="[
+            'px-4 py-2 rounded-full text-sm font-medium transition-colors duration-300',
+            selectedStatus === 'pending'
+              ? 'bg-red-500 text-white'
+              : 'border-2 border-red-500 text-red-500 hover:bg-red-50',
+          ]"
+        >
           New Orders
         </button>
-        <button @click="selectedStatus = 'preparing'" :class="[
-          'px-4 py-2 rounded-full text-sm font-medium transition-colors duration-300',
-          selectedStatus === 'preparing'
-            ? 'bg-yellow-500 text-white'
-            : 'border-2 border-yellow-500 text-yellow-500 hover:bg-yellow-50',
-        ]">
+        <button
+          @click="selectedStatus = 'preparing'"
+          :class="[
+            'px-4 py-2 rounded-full text-sm font-medium transition-colors duration-300',
+            selectedStatus === 'preparing'
+              ? 'bg-yellow-500 text-white'
+              : 'border-2 border-yellow-500 text-yellow-500 hover:bg-yellow-50',
+          ]"
+        >
           Preparing
         </button>
-        <button @click="selectedStatus = 'delivered'" :class="[
-          'px-4 py-2 rounded-full text-sm font-medium transition-colors duration-300',
-          selectedStatus === 'delivered'
-            ? 'bg-green-500 text-white'
-            : 'border-2 border-green-500 text-green-500 hover:bg-green-50',
-        ]">
+        <button
+          @click="selectedStatus = 'delivered'"
+          :class="[
+            'px-4 py-2 rounded-full text-sm font-medium transition-colors duration-300',
+            selectedStatus === 'delivered'
+              ? 'bg-green-500 text-white'
+              : 'border-2 border-green-500 text-green-500 hover:bg-green-50',
+          ]"
+        >
           Delivered
+        </button>
+
+        <button
+          @click="selectedStatus = 'cancelled'"
+          :class="[
+            'px-4 py-2 rounded-full text-sm font-medium transition-colors duration-300',
+            selectedStatus === 'cancelled'
+              ? 'bg-blue-500 text-white'
+              : 'border-2 border-blue-500 text-blue-500 hover:bg-blue-50',
+          ]"
+        >
+          Cancelled
         </button>
       </div>
     </div>
@@ -47,14 +68,20 @@
     </div>
 
     <div class="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 gap-6">
-      <div v-for="order in ordersListSortedAndFiltered" :key="order.id" :class="[
-        'p-4 relative bg-white shadow-md flex flex-col lg:max-w-[362px] min-h-[257px] rounded-lg transition-all duration-300',
-        order.newOrder
-          ? 'ring-2 ring-red-500 ring-offset-4 ring-offset-white scale-105'
-          : '',
-      ]">
-        <div v-if="order.newOrder"
-          class="absolute -top-2 -right-2 bg-red-500 text-white text-xs font-bold px-2 py-1 rounded-full shadow-lg">
+      <div
+        v-for="order in ordersListSortedAndFiltered"
+        :key="order.id"
+        :class="[
+          'p-4 relative bg-white shadow-md flex flex-col lg:max-w-[362px] min-h-[257px] rounded-lg transition-all duration-300',
+          order.newOrder
+            ? 'ring-2 ring-red-500 ring-offset-4 ring-offset-white scale-105'
+            : '',
+        ]"
+      >
+        <div
+          v-if="order.newOrder"
+          class="absolute -top-2 -right-2 bg-red-500 text-white text-xs font-bold px-2 py-1 rounded-full shadow-lg"
+        >
           New
         </div>
         <div class="flex items-center justify-between mb-4">
@@ -64,14 +91,18 @@
               Date: {{ formatDate(order.created_at) }}
             </p>
           </div>
-          <p :class="{
-            'bg-red-500 text-white text-sm rounded-full px-4 py-1 text-center':
-              order.status === 'pending',
-            'bg-yellow-500 text-white text-sm rounded-full px-4 py-1 text-center':
-              order.status === 'preparing',
-            'bg-green-500 text-white text-sm rounded-full px-4 py-1 text-center':
-              order.status === 'delivered',
-          }">
+          <p
+            :class="{
+              'bg-red-500 text-white text-sm rounded-full px-4 py-1 text-center':
+                order.status === 'pending',
+              'bg-yellow-500 text-white text-sm rounded-full px-4 py-1 text-center':
+                order.status === 'preparing',
+              'bg-green-500 text-white text-sm rounded-full px-4 py-1 text-center':
+                order.status === 'delivered',
+              'bg-blue-500 text-white text-sm rounded-full px-4 py-1 text-center':
+                order.status === 'delivered',
+            }"
+          >
             {{ capitalize(order.status) }}
           </p>
         </div>
@@ -104,20 +135,29 @@
           Proceed Order
         </button> -->
 
-        <button @click="openModal(order)"
-          class="button outline-none text-white py-2 px-4 rounded-lg max-w-[154px] min-h-[42px]">
+        <button
+          @click="openModal(order)"
+          class="button outline-none text-white py-2 px-4 rounded-lg max-w-[154px] min-h-[42px]"
+        >
           Proceed Order
         </button>
       </div>
     </div>
 
-    <div v-if="isModalOpen"
-      class="fixed inset-0 z-50 flex items-center justify-center bg-black bg-opacity-50 backdrop-blur-md p-4 md:p-0">
-      <div class="w-full md:w-[800px] relative bg-[#FFFFFF] rounded-lg max-h-[90vh] overflow-hidden">
+    <div
+      v-if="isModalOpen"
+      class="fixed inset-0 z-50 flex items-center justify-center bg-black bg-opacity-50 backdrop-blur-md p-4 md:p-0"
+    >
+      <div
+        class="w-full md:w-[800px] relative bg-[#FFFFFF] rounded-lg max-h-[90vh] overflow-hidden"
+      >
         <!-- Modal Header -->
         <div
-          class="bg-[#F3F4F6] w-full h-auto min-h-[56px] px-4 md:px-6 py-4 flex flex-col md:flex-row items-start md:items-center justify-between gap-4">
-          <div class="font-semibold text-lg md:text-xl flex items-center justify-between w-full md:w-auto">
+          class="bg-[#F3F4F6] w-full h-auto min-h-[56px] px-4 md:px-6 py-4 flex flex-col md:flex-row items-start md:items-center justify-between gap-4"
+        >
+          <div
+            class="font-semibold text-lg md:text-xl flex items-center justify-between w-full md:w-auto"
+          >
             <p>Order ID: {{ selectedOrder.id }}</p>
             <button @click="closeModal" class="md:hidden">
               <img src="/public/close-icon.svg" alt="close" />
@@ -126,27 +166,51 @@
 
           <!-- Status Buttons - Hidden on Mobile -->
           <div class="hidden md:inline-flex gap-4 items-center">
-            <button @click="updateOrderStatus('pending')" :class="{
-              'bg-red-500 text-white': selectedOrder.status === 'pending',
-              'border-red-500 text-red-500':
-                selectedOrder.status !== 'pending',
-            }" class="border-2 px-4 py-1 rounded-full text-center text-sm font-medium">
+            <button
+              @click="updateOrderStatus('pending')"
+              :class="{
+                'bg-red-500 text-white': selectedOrder.status === 'pending',
+                'border-red-500 text-red-500':
+                  selectedOrder.status !== 'pending',
+              }"
+              class="border-2 px-4 py-1 rounded-full text-center text-sm font-medium"
+            >
               Pending
             </button>
-            <button @click="updateOrderStatus('preparing')" :class="{
-              'bg-yellow-500 text-white':
-                selectedOrder.status === 'preparing',
-              'border-yellow-500 text-yellow-500':
-                selectedOrder.status !== 'preparing',
-            }" class="border-2 px-4 py-1 rounded-full text-center text-sm font-medium">
+            <button
+              @click="updateOrderStatus('preparing')"
+              :class="{
+                'bg-yellow-500 text-white':
+                  selectedOrder.status === 'preparing',
+                'border-yellow-500 text-yellow-500':
+                  selectedOrder.status !== 'preparing',
+              }"
+              class="border-2 px-4 py-1 rounded-full text-center text-sm font-medium"
+            >
               Preparing
             </button>
-            <button @click="updateOrderStatus('delivered')" :class="{
-              'bg-green-500 text-white': selectedOrder.status === 'delivered',
-              'border-green-500 text-green-500':
-                selectedOrder.status !== 'delivered',
-            }" class="border-2 px-4 py-1 rounded-full text-center text-sm font-medium">
+            <button
+              @click="updateOrderStatus('delivered')"
+              :class="{
+                'bg-green-500 text-white': selectedOrder.status === 'delivered',
+                'border-green-500 text-green-500':
+                  selectedOrder.status !== 'delivered',
+              }"
+              class="border-2 px-4 py-1 rounded-full text-center text-sm font-medium"
+            >
               Delivered
+            </button>
+
+            <button
+              @click="updateOrderStatus('cancelled')"
+              :class="{
+                'bg-blue-500 text-white': selectedOrder.status === 'cancelled',
+                'border-blue-500 text-blue-500':
+                  selectedOrder.status !== 'cancelled',
+              }"
+              class="border-2 px-4 py-1 rounded-full text-center text-sm font-medium"
+            >
+              Cancelled
             </button>
           </div>
 
@@ -157,7 +221,9 @@
 
         <!-- Customer Info Section -->
         <div class="px-4 md:px-6 py-4">
-          <div class="flex flex-col md:flex-row md:justify-between md:items-center gap-2 md:gap-0">
+          <div
+            class="flex flex-col md:flex-row md:justify-between md:items-center gap-2 md:gap-0"
+          >
             <div class="flex gap-3 items-center">
               <p class="text-[#747474] text-sm">Name:</p>
               <p class="ml-3">{{ selectedOrder.user.name || "No Name" }}</p>
@@ -183,27 +249,43 @@
 
         <!-- Mobile Status Selector -->
         <div class="px-4 md:hidden mb-4">
-          <select v-model="selectedOrder.status" @change="updateOrderStatus($event.target.value)"
-            class="w-full p-3 border-2 rounded-lg focus:outline-none transition-colors duration-300" :class="{
+          <select
+            v-model="selectedOrder.status"
+            @change="updateOrderStatus($event.target.value)"
+            class="w-full p-3 border-2 rounded-lg focus:outline-none transition-colors duration-300"
+            :class="{
               'border-red-500': selectedOrder.status === 'pending',
               'border-yellow-500': selectedOrder.status === 'preparing',
               'border-green-500': selectedOrder.status === 'delivered',
-            }">
+              'border-blue-500': selectedOrder.status === 'cancelled',
+            }"
+          >
             <option value="pending">Pending</option>
             <option value="preparing">Preparing</option>
             <option value="delivered">Delivered</option>
+            <option value="cancelled">Cancelled</option>
           </select>
         </div>
 
         <!-- Order Items Section -->
-        <div class="mt-4 px-4 md:px-6 overflow-y-auto" style="max-height: calc(90vh - 300px)">
+        <div
+          class="mt-4 px-4 md:px-6 overflow-y-auto"
+          style="max-height: calc(90vh - 300px)"
+        >
           <div v-if="selectedOrder.items.length > 0">
-            <div v-for="item in selectedOrder.items" :key="item.id" class="relative mb-4 p-4 bg-[#E5E7EB] rounded-lg">
+            <div
+              v-for="item in selectedOrder.items"
+              :key="item.id"
+              class="relative mb-4 p-4 bg-[#E5E7EB] rounded-lg"
+            >
               <div class="flex flex-col md:flex-row gap-4">
                 <!-- Product Image -->
                 <div class="flex-shrink-0">
-                  <img :src="item.product.image_url" alt="No image"
-                    class="w-full md:w-[100px] h-[100px] object-contain rounded-lg" />
+                  <img
+                    :src="item.product.image_url"
+                    alt="No image"
+                    class="w-full md:w-[100px] h-[100px] object-contain rounded-lg"
+                  />
                 </div>
 
                 <!-- Product Details -->
@@ -246,7 +328,8 @@
           <div class="flex justify-between items-center">
             <div></div>
             <div
-              class="py-3 px-4 bg-[#272727] text-white rounded-lg flex gap-4 md:gap-10 items-center justify-center w-full md:w-auto">
+              class="py-3 px-4 bg-[#272727] text-white rounded-lg flex gap-4 md:gap-10 items-center justify-center w-full md:w-auto"
+            >
               <h1 class="text-sm md:text-base">Total Price:</h1>
               <h1 class="text-sm md:text-base">
                 {{ selectedOrder.total_price }}
@@ -282,12 +365,11 @@ const ordersListSortedAndFiltered = computed(() => {
       ...order,
       newOrderStatus: order.status, // Track initial status
     }))
-    .sort(
-      (a, b) => new Date(b.created_at) - new Date(a.created_at)
-    ).filter((order) => {
+    .sort((a, b) => new Date(b.created_at) - new Date(a.created_at))
+    .filter((order) => {
       if (!!!selectedStatus.value) return true;
 
-      return order.status === selectedStatus.value
+      return order.status === selectedStatus.value;
     });
 });
 
@@ -301,7 +383,6 @@ const fetchRestaurantOrders = async () => {
     loading.value = false;
   }
 };
-
 
 // Function to update order status
 const updateOrderStatus = async (status) => {
@@ -349,7 +430,6 @@ const closeModal = () => {
 watch(selectedStatus, async () => {
   await fetchRestaurantOrders();
 });
-
 
 // Set up WebSocket connection on mounted
 onMounted(async () => {

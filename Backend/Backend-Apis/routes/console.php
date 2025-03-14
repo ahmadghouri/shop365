a5c9bc0c6bef1d41e7ddb81f0975aeddf566d404
Ali Hassan <alihassan478@icloud.com>
2025-03-14 21:32:12 +0500

    database backup implemented

--- Backend/Backend-Apis/routes/console.php
+++ Backend/Backend-Apis/routes/console.php
@@ -2,6 +2,12 @@
 
 use Illuminate\Foundation\Inspiring;
 use Illuminate\Support\Facades\Artisan;
+use Illuminate\Console\Scheduling\Schedule;
+
+$schedule = app(Schedule::class)
+    ->command('backup:send')
+    ->weeklyOn(6, '08:00')
+    ->timezone('Asia/Karachi');
 
 Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());

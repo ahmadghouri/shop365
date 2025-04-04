7e6797513278b8653009fa66c039667f49bfebc9
Muhammad Usman <usmanali730771@gmail.com>
2025-04-05 00:56:03 +0500

    refactor buttons at productdetail page for prescription update order placement logic to handle inactive items and prescription checks

--- Backend/Backend-Apis/database/migrations/2024_07_10_152737_create_products_table.php
+++ Backend/Backend-Apis/database/migrations/2024_07_10_152737_create_products_table.php
@@ -18,7 +18,6 @@ return new class extends Migration
             $table->string('type');
             $table->string('price');
             $table->string('image')->nullable();
-            $table->foreignId('perscription_id')->nullable()->constrained()->cascadeOnDelete()->cascadeOnUpdate();
             $table->foreignId('business_id')->constrained('businesses')->restrictOnDelete()->cascadeOnUpdate();  
             $table->softDeletes();
             $table->timestamps();

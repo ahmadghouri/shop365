28ca9d0f8a3b46569134e9879d928b2eea412ee5
Muhammad Usman <usmanali730771@gmail.com>
2025-04-15 01:27:37 -0700

    Revert "Add Wheel Spinner feature and track new users; update package dependencies"
    This reverts commit 77cc453e781a28115e4bdabdf61c5a8091fcd6c6.

--- Frontend/src/pages/RegisterPage.vue
+++ Frontend/src/pages/RegisterPage.vue
@@ -176,9 +176,6 @@ const register = async () => {
       password: password.value,
     });
 
-    // Set new user flag in localStorage
-    localStorage.setItem('isNewUser', 'true');
-
     const authStore = useAuthStore();
     const cartStore = useCartStore();
 

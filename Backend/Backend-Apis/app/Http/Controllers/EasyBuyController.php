b05cf68944c1f32027576d8e3c868b48aa053939
Muhammad Usman <usmanali730771@gmail.com>
2025-07-25 02:10:24 +0500

    easybuy mrc

---
+++ Backend/Backend-Apis/app/Http/Controllers/EasyBuyController.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\EasyBuy;
+use Illuminate\Http\Request;
+
+class EasyBuyController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(EasyBuy $easyBuy)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(EasyBuy $easyBuy)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, EasyBuy $easyBuy)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(EasyBuy $easyBuy)
+    {
+        //
+    }
+}

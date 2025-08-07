00bcb665e39ee4eea2f460f5c75e5af69b1b2332
Muhammad Usman <usmanali730771@gmail.com>
2025-08-07 19:36:48 +0500

    fix fr production

--- Frontend/src/pages/EasyBuyPage.vue
+++ Frontend/src/pages/EasyBuyPage.vue
@@ -45,7 +45,7 @@
                 <!-- Product Header -->
                 <div class="flex items-center gap-2.5 mb-2.5">
                     <div class="max-w-8 md:max-w-16 max-h-8 md:max-h-16 rounded-lg flex items-center justify-center">
-                        <img src="/Groccery1.png" alt="product image" class="w-full h-full object-cover rounded-lg" />
+                        <img :src="item.image_url" alt="product image" class="w-full h-full object-cover rounded-lg" />
                     </div>
                     <div class="flex justify-between items-center w-full">
                         <h2 class="text-base font-semibold md:text-xl md:font-bold text-[#1E293B]">{{ item.title }}</h2>

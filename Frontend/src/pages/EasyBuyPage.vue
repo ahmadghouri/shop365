309b28cfb0fe727e4bf097f07d37d1c88b18dcb1
Muhammad Usman <usmanali730771@gmail.com>
2025-08-11 20:09:16 +0500

    ui fixes in easybuy page

--- Frontend/src/pages/EasyBuyPage.vue
+++ Frontend/src/pages/EasyBuyPage.vue
@@ -42,7 +42,7 @@
         </div>
 
         <!-- Horizontal Scrollable Filter Section -->
-        <div class="overflow-x-auto whitespace-nowrap mb-8 md:flex justify-center items-center">
+        <div class="overflow-x-auto whitespace-nowrap mb-8">
             <button v-for="item in filters" :key="item.id" @click="onFilterClick(item)" :class="[
                 item === selectedFilter
                     ? 'inline-block px-4 py-2 mx-2 text-sm font-medium rounded-full cursor-pointer bg-yellow-500 text-white hover:bg-yellow-700'
@@ -55,7 +55,8 @@
         <!-- Easy Buy Items -->
         <div class="flex flex-col gap-3 md:gap-6 relative">
             <div v-for="item in groceryItems" :key="item.id"
-                class="w-full md:w-2/3 xl:w-1/2 mb-4 bg-white rounded-xl md:rounded-2xl p-4 shadow-[0px_0px_10px_0px_rgba(0,0,0,0.25)] mx-auto">
+                class="w-full md:w-2/3 xl:w-1/2 mb-4 bg-white rounded-xl md:rounded-2xl p-4 
+                shadow-[0px_0px_10px_0px_rgba(0,0,0,0.25)] mx-auto">
                 <!-- Product Header -->
                 <div class="flex items-center gap-2.5 mb-2.5">
                     <div class="max-w-8 md:max-w-16 max-h-8 md:max-h-16 rounded-lg flex items-center justify-center">
@@ -70,13 +71,15 @@
                 </div>
 
                 <!-- Main Selection -->
-                <div class="selection-section flex justify-between gap-5 md:gap-10 items-center mb-2.5 md:mb-4">
+                <div class="selection-section flex flex-row justify-between gap-2 sm:gap-4 md:gap-6 items-center 
+                    mb-2.5 md:mb-4 max-w-full">
                     <!-- Brand Selection -->
-                    <div class="w-full max-w-36 sm:max-w-none flex flex-col gap-1.5">
-                        <label for="brand" class="text-sm md:text-lg font-semibold">Brand</label>
+                    <div class="flex flex-col gap-1.5 flex-1 min-w-0">
+                        <label :for="`brand-${item.id}`" class="text-sm md:text-lg font-semibold">Brand</label>
                         <select :id="`brand-${item.id}`" v-model="currentSelections[item.id].selectedBrand"
                             @change="onBrandChange(item.id)"
-                            class="border border-[#0000001A] rounded px-3 py-1 md:py-2 text-xs md:text-base leading-4 cursor-pointer">
+                            class="w-full border border-[#0000001A] rounded px-2 sm:px-3 py-1 md:py-2 
+                            text-xs md:text-base leading-4 cursor-pointer">
                             <option value="" disabled>Choose Brand</option>
                             <option v-for="brand in Object.keys(item.payload)" :key="brand" :value="brand">
                                 {{ brand }}
@@ -85,11 +88,12 @@
                     </div>
 
                     <!-- Quantity Selection -->
-                    <div class="w-full max-w-36 sm:max-w-none flex flex-col gap-1.5">
-                        <label for="quantity" class="text-sm md:text-lg font-semibold">Quantity</label>
+                    <div class="flex flex-col gap-1.5 flex-1 min-w-0">
+                        <label :for="`quantity-${item.id}`" class="text-sm md:text-lg font-semibold">Quantity</label>
                         <select :id="`quantity-${item.id}`" v-model="currentSelections[item.id].selectedQuantity"
                             :disabled="!currentSelections[item.id].selectedBrand"
-                            class="border border-[#0000001A] rounded px-3 py-1 md:py-2 text-xs md:text-base leading-4 cursor-pointer disabled:bg-gray-100">
+                            class="w-full border border-[#0000001A] rounded px-2 sm:px-3 py-1 md:py-2 
+                            text-xs md:text-base leading-4 cursor-pointer disabled:bg-gray-100">
                             <option value="" disabled>Choose quantity</option>
                             <option v-for="quantity in getAvailableQuantities(item.id)" :key="quantity"
                                 :value="quantity">
